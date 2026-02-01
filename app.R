6eab520548b0193ca579d9c691c24fc8cfd2abb2
BD157 <brindadeep7@gmail.com>
2026-01-31 21:09:51 -0500

    added app code

--- app.R
+++ app.R
@@ -1,2 +1,23 @@
 
+library(shiny)
+source("R/openai_client.R")
 
+ui <- fluidPage(
+  textAreaInput("question", "Ask GPT"),
+  actionButton("ask", "Submit"),
+  verbatimTextOutput("answer")
+)
+
+server <- function(input, output) {
+
+  observeEvent(input$ask, {
+
+    output$answer <- renderText({
+      call_chatgpt(input$question)
+    })
+
+  })
+
+}
+
+shinyApp(ui, server)
